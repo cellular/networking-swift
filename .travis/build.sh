025fcb2b250afd3487d02256c3dfa649614a4bc3
Markus Winter <markus@wntr.me>
2018-11-05 21:15:59 +0100

    Added github and travis files

---
+++ .travis/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e; # Fail on first error
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+
+    export PATH="$HOME/.swiftenv/bin:$HOME/.swiftenv/shims:$PATH";
+    swift build; swift build -c release; swift test;
+
+else # iOS | watchOS | tvOS | macOS
+
+    # Prepare
+    set -o pipefail; # xcpretty
+    xcodebuild -version;
+    xcodebuild -showsdks;
+
+    # Build Framework in Debug and Run Tests if specified
+    if [ $RUN_TESTS == "YES" ]; then
+        xcodebuild -workspace "CellularNetworking.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
+    else
+        xcodebuild -workspace "CellularNetworking.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO build | xcpretty;
+    fi
+    # Build Framework in Release and Run Tests if specified
+    if [ $RUN_TESTS == "YES" ]; then
+        xcodebuild -workspace "CellularNetworking.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
+    else
+        xcodebuild -workspace "CellularNetworking.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO build | xcpretty;
+    fi
+    # Run `pod lib lint` if specified
+    if [ $POD_LINT == "YES" ]; then
+        pod lib lint;
+    fi
+fi
