1bde632398c237ec961f4aef2bf415102e44366f
Markus Winter <markus@wntr.me>
2018-11-05 21:24:57 +0100

    Update build related scripts

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 set -e; # Fail on first error
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+# Prepare
+set -o pipefail; # xcpretty
+xcodebuild -version;
+xcodebuild -showsdks;
 
-    export PATH="$HOME/.swiftenv/bin:$HOME/.swiftenv/shims:$PATH";
-    swift build; swift build -c release; swift test;
-
-else # iOS | watchOS | tvOS | macOS
-
-    # Prepare
-    set -o pipefail; # xcpretty
-    xcodebuild -version;
-    xcodebuild -showsdks;
-
-    # Build Framework in Debug and Run Tests if specified
-    if [ $RUN_TESTS == "YES" ]; then
-        xcodebuild -workspace "CellularNetworking.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
-    else
-        xcodebuild -workspace "CellularNetworking.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO build | xcpretty;
-    fi
-    # Build Framework in Release and Run Tests if specified
-    if [ $RUN_TESTS == "YES" ]; then
-        xcodebuild -workspace "CellularNetworking.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
-    else
-        xcodebuild -workspace "CellularNetworking.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO build | xcpretty;
-    fi
-    # Run `pod lib lint` if specified
-    if [ $POD_LINT == "YES" ]; then
-        pod lib lint;
-    fi
+# Build Framework in Debug and Run Tests if specified
+if [ $RUN_TESTS == "YES" ]; then
+    xcodebuild -workspace "CellularNetworking.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
+else
+    xcodebuild -workspace "CellularNetworking.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO build | xcpretty;
+fi
+# Build Framework in Release and Run Tests if specified
+if [ $RUN_TESTS == "YES" ]; then
+    xcodebuild -workspace "CellularNetworking.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
+else
+    xcodebuild -workspace "CellularNetworking.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO build | xcpretty;
+fi
+# Run `pod lib lint` if specified
+if [ $POD_LINT == "YES" ]; then
+    pod lib lint;
 fi
