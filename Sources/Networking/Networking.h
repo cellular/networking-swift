67084ded47207310ff44c2a93b4aece2c9b0253c
Markus Winter <markus@wntr.me>
2018-11-11 21:07:47 +0100

    Remove an invalid header

--- Sources/Networking/Networking.h
+++ Sources/Networking/Networking.h
@@ -1,11 +1,3 @@
-//
-//  Networking.h
-//  Networking
-//
-//  Created by Markus Winter on 06.11.18.
-//  Copyright © 2018 CELLULAR GmbH. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 //! Project version number for Networking.
